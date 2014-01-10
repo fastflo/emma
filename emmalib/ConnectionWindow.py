d4f667aaab261e1f6cd278c22d77c1f9669136bf
MrElectroNick <mr.electronick@gmail.com>
2014-01-11 01:23:24 +0200

    Rework of connection window... in progress... part 2

--- emmalib/ConnectionWindow.py
+++ emmalib/ConnectionWindow.py
@@ -15,19 +15,18 @@ class ConnectionWindow:
         self.text_fields = ["name", "host", "port", "user", "password", "database"]
         self.glade_file = os.path.join(emma_path, "ConnectionWindow.glade")
         self.glade = gtk.glade.XML(self.glade_file)
-        self.window = self.gw('connection_window')
-        print self.window
-        self.gw("cw_apply_button").connect("clicked", self.on_apply_button_clicked)
-        self.gw("cw_test_button").connect("clicked", self.on_test_button_clicked)
+        self.window = self.connection_window
+        self.cw_apply_button.connect("clicked", self.on_apply_button_clicked)
+        self.cw_test_button.connect("clicked", self.on_test_button_clicked)
         self.show_mysql()
-        self.gw("cmb_connection_type").connect("changed", self.on_connection_type_changed)
+        self.cmb_connection_type.connect("changed", self.on_connection_type_changed)
         if self.emma:
             self.window.connect('destroy', self.on_window_close)
             self.window.connect('delete-event', self.on_window_delete)
-            self.gw("cw_abort_button").connect("clicked", self.on_window_close)
+            self.cw_abort_button.connect("clicked", self.on_window_close)
         else:
             self.window.connect('destroy', lambda *a: gtk.main_quit())
-            self.gw("cw_abort_button").connect("clicked", lambda *a: gtk.main_quit())
+            self.cw_abort_button.connect("clicked", lambda *a: gtk.main_quit())
             self.window.show()
 
     def on_window_delete(self, *args):
@@ -39,31 +38,39 @@ class ConnectionWindow:
         return False
 
     def show_mysql(self):
-        self.gw("lb_provider_name").set_text('MySQL')
-        self.gw("cmb_connection_type").set_active(0)
+        self.lb_provider_name.set_text('MySQL')
+        self.cmb_connection_type.set_active(0)
         for n in ['host', 'port', 'user', 'password', 'database']:
-            self.gw("lb_%s" % n).show()
-            self.gw("tb_%s" % n).show()
-        self.gw("lb_datafile").hide()
-        self.gw("fcb_datafile").hide()
-        self.gw("lb_datafile_current").hide()
-        self.gw("lb_datafile_current_path").hide()
+            self.glade.get_widget("lb_%s" % n).show()
+            self.glade.get_widget("tb_%s" % n).show()
+        self.lb_datafile.hide()
+        self.fcb_datafile.hide()
+        self.lb_datafile_current.hide()
+        self.lb_datafile_current_path.hide()
+        self.cw_test_button.show()
 
     def show_sqlite(self):
-        self.gw("lb_provider_name").set_text('SQLite')
-        self.gw("cmb_connection_type").set_active(1)
+        self.lb_provider_name.set_text('SQLite')
+        self.cmb_connection_type.set_active(1)
         for n in ['host', 'port', 'user', 'password', 'database']:
-            self.gw("lb_%s" % n).hide()
-            self.gw("tb_%s" % n).hide()
-        self.gw("lb_datafile").show()
-        self.gw("fcb_datafile").show()
-        self.gw("lb_datafile_current").show()
-        self.gw("lb_datafile_current_path").show()
+            self.glade.get_widget("lb_%s" % n).hide()
+            self.glade.get_widget("tb_%s" % n).hide()
+        if self.cw_mode == "new":
+            self.lb_datafile.show()
+            self.fcb_datafile.show()
+            self.lb_datafile_current.hide()
+            self.lb_datafile_current_path.hide()
+        else:
+            self.lb_datafile.hide()
+            self.fcb_datafile.hide()
+            self.lb_datafile_current.show()
+            self.lb_datafile_current_path.show()
+        self.cw_test_button.hide()
 
     def cleanup(self):
-        self.gw("lb_provider_name").set_text('')
+        self.lb_provider_name.set_text('')
         for n in self.text_fields:
-            self.gw("tb_%s" % n).set_text('')
+            self.glade.get_widget("tb_%s" % n).set_text('')
 
     def on_connection_type_changed(self, cmb):
         if cmb.get_active() == 0:
@@ -71,38 +78,40 @@ class ConnectionWindow:
         else:
             self.show_sqlite()
 
-    def gw(self, wn):
-        return self.glade.get_widget(wn)
-
     def show(self, mode):
         self.cleanup()
         if mode == "edit":
-            self.gw("cmb_connection_type").hide()
-            self.gw("lb_provider_name").show()
+            self.cmb_connection_type.hide()
+            self.lb_provider_name.show()
             self.window.set_title('Edit connection')
             if self.host.__class__.__name__ == 'MySqlHost':
-                self.gw("tb_name").set_text(self.host.name)
-                self.gw("tb_host").set_text(self.host.host)
-                self.gw("tb_port").set_text(self.host.port)
-                self.gw("tb_user").set_text(self.host.user)
-                self.gw("tb_password").set_text(self.host.password)
-                self.gw("tb_database").set_text(self.host.database)
+                self.tb_name.set_text(self.host.name)
+                self.tb_host.set_text(self.host.host)
+                self.tb_port.set_text(self.host.port)
+                self.tb_user.set_text(self.host.user)
+                self.tb_password.set_text(self.host.password)
+                self.tb_database.set_text(self.host.database)
                 self.show_mysql()
             else:
-                self.gw("lb_datafile_current_path").set_title(self.host.name)
+                self.tb_name.set_text(self.host.name)
+                self.lb_datafile.hide()
+                self.fcb_datafile.hide()
+                self.lb_datafile_current.show()
+                self.lb_datafile_current_path.show()
+                self.lb_datafile_current_path.set_text(self.host.name)
                 self.show_sqlite()
         else:
-            self.gw("cmb_connection_type").show()
-            self.gw("lb_provider_name").hide()
+            self.cmb_connection_type.show()
+            self.lb_provider_name.hide()
             self.window.set_title('New connection')
             for n in ['name', 'host', 'port', 'user', 'password', 'database']:
-                self.gw("tb_%s" % n).set_text('')
+                self.glade.get_widget("tb_%s" % n).set_text('')
 
         self.cw_mode = mode
         self.window.show()
 
     def validate_mysql(self):
-        if not self.gw('tb_name').get_text():
+        if not self.glade.get_widget('tb_name').get_text():
             dialogs.alert('Please enter connection name!')
             return False
 
@@ -110,13 +119,13 @@ class ConnectionWindow:
         return True
 
     def on_apply_button_clicked(self, *args):
-        if self.gw("cmb_connection_type").get_active() == 0:
+        if self.cmb_connection_type.get_active() == 0:
             if not self.validate_mysql():
                 return
             if self.cw_mode == "new":
                 data = []
                 for n in self.text_fields:
-                    data.append(self.gw("tb_%s" % n).get_text())
+                    data.append(self.glade.get_widget("tb_%s" % n).get_text())
                 print data
                 if not data[0]:
                     self.window.hide()
@@ -124,14 +133,17 @@ class ConnectionWindow:
                 if self.emma:
                     self.emma.add_mysql_host(*data)
             else:
-                self.host.name = self.gw("tb_name").get_text()
-                self.host.host = self.gw("tb_host").get_text()
-                self.host.port = self.gw("tb_port").get_text()
-                self.host.user = self.gw("tb_user").get_text()
-                self.host.password = self.gw("tb_password").get_text()
-                self.host.database = self.gw("tb_database").get_text()
+                self.host.name = self.tb_name.get_text()
+                self.host.host = self.tb_host.get_text()
+                self.host.port = self.tb_port.get_text()
+                self.host.user = self.tb_user.get_text()
+                self.host.password = self.tb_password.get_text()
+                self.host.database = self.tb_database.get_text()
         else:
-            dialogs.alert('Save SQLite')
+            if self.cw_mode == "new":
+                pass
+            else:
+                self.emma.add_sqlite(self.fcb_datafile.get_filename())
 
         self.window.hide()
 
@@ -139,7 +151,7 @@ class ConnectionWindow:
             self.emma.save_config()
 
     def on_test_button_clicked(self, *args):
-        if self.gw("cmb_connection_type").get_active() == 0:
+        if self.cmb_connection_type.get_active() == 0:
             if not self.validate_mysql():
                 return
             import _mysql
@@ -151,12 +163,12 @@ class ConnectionWindow:
             }
             for n in ["host", "user", "password", "port"]:
                 if n == 'port':
-                    port = self.gw('tb_port').get_text()
+                    port = self.glade.get_widget('tb_port').get_text()
                     if not port:
                         port = '3306'
                     data[widget_map.get(n, n)] = int(port)
                 else:
-                    data[widget_map.get(n, n)] = self.gw("tb_%s" % n).get_text()
+                    data[widget_map.get(n, n)] = self.glade.get_widget("tb_%s" % n).get_text()
             try:
                 handle = _mysql.connect(**data)
             except _mysql.DatabaseError as err:
@@ -177,7 +189,13 @@ class ConnectionWindow:
             )
             handle.close()
         else:
-            dialogs.alert('Test SQLite')
+            dialogs.alert('Nothing to test')
+
+    def __getattr__(self, name):
+        widget = self.glade.get_widget(name)
+        if widget is None:
+            raise AttributeError(name)
+        return widget
 
 
 if __name__ == "__main__":
