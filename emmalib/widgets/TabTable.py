8cc2562d5bb1a641ddc811352483a6176f99a5c0
MrElectroNick <mr.electronick@gmail.com>
2014-05-11 16:25:16 +0300

    Working on proper result view sorting and display - seems like finally working

--- emmalib/widgets/TabTable.py
+++ emmalib/widgets/TabTable.py
@@ -85,76 +85,111 @@ class TabTable(BaseTab):
             self.load_data()
 
     def load_data(self):
-        self.data_loaded = True
         result = self.table.db.host.query_dict('SELECT * FROM %s' % self.table.name, append_to_log=False, encoding='utf8')
-
         if not result:
             return
-
+        #
+        #   Build list store with sort and data columns
+        #   Make map of result column index, its display column and sort column
+        #
         columns = []
+        sort_display_map = []
+        i = 0
+        ci = 0
         for t in result['types']:
             columns.append(t)
-
-        print columns
-
+            sort_index = i
+            disp_index = sort_index
+            i += 1
+            if t == gobject.TYPE_LONG or t == gobject.TYPE_INT or t == gobject.TYPE_FLOAT:
+                columns.append(gobject.TYPE_STRING)
+                disp_index = i
+                i += 1
+            sort_display_map.append((ci, sort_index, disp_index))
+            ci += 1
+        #
+        #   Add it all to model and in treeview
+        #
         self.tv_data_model = gtk.ListStore(*columns)
-
         self.tv_data.set_model(self.tv_data_model)
+        #
+        #   Add treeview columns according to previously built index/sort/data map
+        #
+        for column_index, sort_index, disp_index in sort_display_map:
+            title = result['cols'][column_index].replace("_", "__").replace("[\r\n\t ]+", " ")
+            self.column_insert(title, sort_index, disp_index)
 
-        for i in range(len(result['cols'])):
-            title = result['cols'][i].replace("_", "__").replace("[\r\n\t ]+", " ")
-            field_type = result['types'][i]
-
-            text_renderer = gtk.CellRendererText()
-            if True:  # editable
-                text_renderer.set_property("editable", True)
-                #text_renderer.connect("edited", self.on_query_change_data, i)
-
-            l = self.tv_data.insert_column_with_data_func(
-                -1, title, text_renderer, render_mysql_string, i)
-
-            col = self.tv_data.get_column(l - 1)
-            col.set_sort_column_id(l-1)
-
-            # if self.emma.config.get_bool("result_view_column_resizable"):
-            #     col.set_resizable(True)
-            # else:
-            #     col.set_resizable(False)
-            #     col.set_min_width(int(self.emma.config.get("result_view_column_width_min")))
-            #     col.set_max_width(int(self.emma.config.get("result_view_column_width_max")))
-            #
-            # if True:  # sortable
-            #     col.set_clickable(True)
-            #     col.set_sort_indicator(True)
-            # else:
-            #     col.set_clickable(False)
-            #     col.set_sort_indicator(False)
-
+        #
+        #   Fill in List store with data
+        #   In case of long and int columns - two columns added
+        #   to make sorting and rendering properly
+        #
         for row in result['rows']:
             model_row = []
             for col in result['cols']:
-                v = None
-                if row[col] is not None:
-                    ci = result['cols'].index(col)
-                    if result['types'][ci] == gobject.TYPE_LONG:
+                ci = result['cols'].index(col)
+                if result['types'][ci] == gobject.TYPE_LONG:
+                    if row[col] is None:
+                        v = 0
+                    else:
                         v = long(row[col])
-                    elif result['types'][ci] == gobject.TYPE_INT:
+                    model_row.append(v)
+                    model_row.append(row[col])
+                elif result['types'][ci] == gobject.TYPE_INT:
+                    if row[col] is None:
+                        v = 0
+                    else:
                         v = int(row[col])
-                    elif result['types'][ci] == gobject.TYPE_STRING:
+                    model_row.append(v)
+                    model_row.append(row[col])
+                elif result['types'][ci] == gobject.TYPE_FLOAT:
+                    if row[col] is None:
+                        v = 0
+                    else:
+                        v = float(row[col])
+                    model_row.append(v)
+                    model_row.append(row[col])
+                elif result['types'][ci] == gobject.TYPE_STRING:
+                    if row[col] is None:
+                        #
+                        #   Add as none
+                        #
+                        model_row.append(None)
+                    else:
                         try:
+                            #
+                            #   Add as ascii
+                            #
                             row[col].decode('ascii')
                             v = str(row[col])
                         except:
                             try:
+                                #
+                                #   Add as converted utf8
+                                #
                                 v = row[col].decode('utf-8')
-                                #v = unicode(row[col])
                             except UnicodeError:
+                                #
+                                #   Can't convert to string
+                                #   Then it might be a binary
+                                #
                                 v = '<BINARY>'
-                model_row.append(v)
+                        model_row.append(v)
             try:
                 self.tv_data_model.append(model_row)
             except:
                 print "cannot add row:", model_row
+
+    def column_insert(self, title, sort_column_index, display_column_index):
+        text_renderer = gtk.CellRendererText()
+        ci = self.tv_data.insert_column_with_data_func(
+            -1,
+            title,
+            text_renderer,
+            render_mysql_string,
+            display_column_index)
+        col = self.tv_data.get_column(ci-1)
+        col.set_sort_column_id(sort_column_index)
 
     def update(self):
         th = self.table
---
+++ tests/text_tv.py
@@ -0,0 +1,131 @@
+import gtk
+import gobject
+from emmalib.providers.mysql import MySqlHost, MySqlTable
+from emmalib.widgets.ResultCellRenders import render_mysql_string
+
+
+class Win(gtk.Window):
+
+    def __init__(self):
+        super(Win, self).__init__()
+        self.sw = gtk.ScrolledWindow()
+        self.sw.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+        self.add(self.sw)
+        self.tv_data = gtk.TreeView()
+        self.sw.add(self.tv_data)
+        self.set_position(gtk.WIN_POS_CENTER)
+        self.resize(640, 480)
+        self.connect('destroy', lambda *args: gtk.main_quit())
+        self.show_all()
+        self.tv_data_model = None
+        self.host = None
+        self.table = None
+        self.db_connect()
+        self.load_data()
+
+    def db_connect(self):
+        _db = 'bohprod'
+        _tb = 'boh_users'
+        self.host = MySqlHost(None, None, 'Localhost', 'localhost', 3306, 'root', 'root', '', 0)
+        self.host.connect()
+        self.host.use_db(_db)
+        self.host.databases[_db].refresh()
+        self.host.databases[_db].tables[_tb].refresh()
+        self.table = self.host.databases[_db].tables[_tb]
+
+    def load_data(self):
+        result = self.table.db.host.query_dict('SELECT * FROM %s' % self.table.name, append_to_log=False, encoding='utf8')
+        if not result:
+            return
+        #
+        #   Build list store with sort and data columns
+        #   Make map of result column index, its display column and sort column
+        #
+        columns = []
+        sort_display_map = []
+        i = 0
+        ci = 0
+        for t in result['types']:
+            columns.append(t)
+            sort_index = i
+            disp_index = sort_index
+            i += 1
+            if t == gobject.TYPE_LONG or t == gobject.TYPE_INT or t == gobject.TYPE_FLOAT:
+                columns.append(gobject.TYPE_STRING)
+                disp_index = i
+                i += 1
+            sort_display_map.append((ci, sort_index, disp_index))
+            ci += 1
+
+        print 'columns', columns
+        print 'sort_display_map', sort_display_map
+
+        #
+        #   Add it all to model and in treeview
+        #
+        self.tv_data_model = gtk.ListStore(*columns)
+        self.tv_data.set_model(self.tv_data_model)
+        #
+        #   Add treeview columns according to previously built index/sort/data map
+        #
+        for column_index, sort_index, disp_index in sort_display_map:
+            title = result['cols'][column_index].replace("_", "__").replace("[\r\n\t ]+", " ")
+            self.column_insert(title, sort_index, disp_index)
+
+        for row in result['rows']:
+            model_row = []
+            for col in result['cols']:
+                ci = result['cols'].index(col)
+                if result['types'][ci] == gobject.TYPE_LONG:
+                    if row[col] is None:
+                        v = 0
+                    else:
+                        v = long(row[col])
+                    model_row.append(v)
+                    model_row.append(row[col])
+                elif result['types'][ci] == gobject.TYPE_INT:
+                    if row[col] is None:
+                        v = 0
+                    else:
+                        v = int(row[col])
+                    model_row.append(v)
+                    model_row.append(row[col])
+                elif result['types'][ci] == gobject.TYPE_FLOAT:
+                    if row[col] is None:
+                        v = 0
+                    else:
+                        v = float(row[col])
+                    model_row.append(v)
+                    model_row.append(row[col])
+                elif result['types'][ci] == gobject.TYPE_STRING:
+                    if row[col] is None:
+                        model_row.append(None)
+                    else:
+                        try:
+                            row[col].decode('ascii')
+                            v = str(row[col])
+                        except:
+                            try:
+                                v = row[col].decode('utf-8')
+                            except UnicodeError:
+                                v = '<BINARY>'
+                        model_row.append(v)
+            try:
+                self.tv_data_model.append(model_row)
+            except:
+                pass
+                #print "cannot add row:", model_row
+
+    def column_insert(self, title, sort_column_index, display_column_index):
+        text_renderer = gtk.CellRendererText()
+        ci = self.tv_data.insert_column_with_data_func(
+            -1,
+            title,
+            text_renderer,
+            render_mysql_string,
+            display_column_index)
+        col = self.tv_data.get_column(ci-1)
+        col.set_sort_column_id(sort_column_index)
+
+w = Win()
+gtk.main()
