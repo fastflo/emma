90fd33dbc5383fdb4bc24abe6b6b7692cb2f9047
MrElectroNick <mr.electronick@gmail.com>
2014-05-18 16:34:59 +0300

    Edit table fields continued Mysql collations dictionary Name, Collation, Engine, Autoincrement, Row format and comment edit fields

--- emmalib/providers/mysql/widgets/TableProperties.py
+++ emmalib/providers/mysql/widgets/TableProperties.py
@@ -19,28 +19,110 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
 
 import gtk
+from collations import collations
 
 
 class TableProperties(gtk.ScrolledWindow):
 
     def __init__(self, table):
         super(TableProperties, self).__init__()
-
+        self.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+        self.set_border_width(8)
         self.table = table
 
-        self.table_description_size = (0, 0)
-        self.table_property_labels = []
-        self.table_property_entries = []
+        self.tb_name = gtk.Entry()
+        self.tb_ai = gtk.Entry()
+        self.tb_comment = gtk.Entry()
+
+        self.cb_engine = gtk.combo_box_new_text()
+        self.cb_engine.append_text('InnoDB')
+        self.cb_engine.append_text('MyISAM')
+
+        self.cb_collation = gtk.combo_box_new_text()
+        for c in collations:
+            self.cb_collation.append_text(c)
+
+        self.cb_rowformat = gtk.combo_box_new_text()
+        self.cb_rowformat.append_text('Compact')
+        self.cb_rowformat.append_text('Redundant')
+
+        self.btn_update = gtk.Button('Update')
 
         vptp = gtk.Viewport()
-        self.table_properties = gtk.Table()
-        vptp.add(self.table_properties)
-        self.add(vptp)
-        self.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
 
+        hbox = gtk.HBox()
+
+        hbox.pack_start(self.build_ltable())
+        hbox.pack_end(self.build_rtable())
+        vptp.add(hbox)
+        vptp.set_shadow_type(gtk.SHADOW_NONE)
+        self.add(vptp)
         self.show_all()
 
+        self.properties = dict(zip(self.table.db.status_headers, self.table.props))
+        for p in self.properties:
+            print p, self.properties[p]
+
+    def build_ltable(self):
+        tbl = gtk.Table(4, 2)
+
+        r = 0
+        tbl.attach(self.mklbl('Name'), 0, 1, r, r+1, gtk.FILL, 0)
+        tbl.attach(self.tb_name, 1, 2, r, r+1, gtk.FILL, 0)
+
+        r += 1
+        tbl.attach(self.mklbl('Engine'), 0, 1, r, r+1, gtk.FILL, 0)
+        tbl.attach(self.cb_engine, 1, 2, r, r+1, gtk.FILL, 0)
+
+        r += 1
+        tbl.attach(self.mklbl('Collation'), 0, 1, r, r+1, gtk.FILL, 0)
+        tbl.attach(self.cb_collation, 1, 2, r, r+1, gtk.FILL, 0)
+
+        r += 1
+        tbl.attach(self.mklbl('Auto increment'), 0, 1, r, r+1, gtk.FILL, 0)
+        tbl.attach(self.tb_ai, 1, 2, r, r+1, gtk.FILL, 0)
+
+        r += 1
+        tbl.attach(self.mklbl('Row format'), 0, 1, r, r+1, gtk.FILL, 0)
+        tbl.attach(self.cb_rowformat, 1, 2, r, r+1, gtk.FILL, 0)
+
+        r += 1
+        tbl.attach(self.mklbl('Comment'), 0, 1, r, r+1, gtk.FILL, 0)
+        tbl.attach(self.tb_comment, 1, 2, r, r+1, gtk.FILL, 0)
+
+        r += 1
+        tbl.attach(self.btn_update, 1, 2, r, r+1, gtk.FILL, 0)
+
+        return tbl
+
+    def mklbl(self, text):
+        lbl = gtk.Label(text)
+        lbl.set_justify(gtk.JUSTIFY_RIGHT)
+        return lbl
+
+    def build_rtable(self):
+        r_table = gtk.Table(1, 2)
+        r_table.attach(gtk.Label('aaa'), 0, 1, 0, 1, gtk.FILL, 0)
+        r_table.attach(gtk.Label('bbb'), 1, 2, 0, 1, gtk.FILL, 0)
+        return r_table
+
     def update(self):
+        self.tb_name.set_text(self.properties['Name'])
+        self.tb_ai.set_text(self.properties['Auto_increment'])
+        self.tb_comment.set_text(self.properties['Comment'])
+
+        self.selcb(self.cb_engine, self.properties['Engine'])
+        self.selcb(self.cb_collation, self.properties['Collation'])
+        self.selcb(self.cb_rowformat, self.properties['Row_format'])
+
+    def selcb(self, cb, text):
+        ix = 0
+        for i in cb.get_model():
+            if i[0] == text:
+                cb.set_active(ix)
+            ix += 1
+
+    def update_old(self):
         table = self.table_properties
         prop_count = len(self.table.props)
         if len(self.table_property_labels) != prop_count:
---
+++ emmalib/providers/mysql/widgets/collations.py
@@ -0,0 +1,271 @@
+collations = {
+    "armscii8_bin": "Armenian, Binary",
+    "armscii8_general_ci": "Armenian, case-insensitive",
+
+    "ascii_bin": "West European (multilingual), Binary",
+    "ascii_general_ci": "West European (multilingual), case-insensitive",
+
+    "big5_bin": "Traditional Chinese, Binary",
+    "big5_chinese_ci": "Traditional Chinese, case-insensitive",
+
+    "binary": "Binary",
+
+    "cp1250_bin": "Central European (multilingual), Binary",
+    "cp1250_croatian_ci": "Croatian, case-insensitive",
+    "cp1250_czech_cs": "Czech, case-sensitive",
+    "cp1250_general_ci": "Central European (multilingual), case-insensitive",
+    "cp1250_polish_ci": "Polish, case-insensitive",
+
+    "cp1251_bin": "Cyrillic (multilingual), Binary",
+    "cp1251_bulgarian_ci": "Bulgarian, case-insensitive",
+    "cp1251_general_ci": "Cyrillic (multilingual), case-insensitive",
+    "cp1251_general_cs": "Cyrillic (multilingual), case-sensitive",
+    "cp1251_ukrainian_ci": "Ukrainian, case-insensitive",
+
+
+    "cp1256_bin": "Arabic, Binary",
+    "cp1256_general_ci": "Arabic, case-insensitive",
+
+
+    "cp1257_bin": "Baltic (multilingual), Binary",
+    "cp1257_general_ci": "Baltic (multilingual), case-insensitive",
+    "cp1257_lithuanian_ci": "Lithuanian, case-insensitive",
+
+
+    "cp850_bin": "West European (multilingual), Binary",
+    "cp850_general_ci": "West European (multilingual), case-insensitive",
+
+
+    "cp852_bin": "Central European (multilingual), Binary",
+    "cp852_general_ci": "Central European (multilingual), case-insensitive",
+
+
+    "cp866_bin": "Russian, Binary",
+    "cp866_general_ci": "Russian, case-insensitive",
+
+
+    "cp932_bin": "Japanese, Binary",
+    "cp932_japanese_ci": "Japanese, case-insensitive",
+
+
+    "dec8_bin": "West European (multilingual), Binary",
+    "dec8_swedish_ci": "Swedish, case-insensitive",
+
+
+    "eucjpms_bin": "Japanese, Binary",
+    "eucjpms_japanese_ci": "Japanese, case-insensitive",
+
+
+    "euckr_bin": "Korean, Binary",
+    "euckr_korean_ci": "Korean, case-insensitive",
+
+
+    "gb2312_bin": "Simplified Chinese, Binary",
+    "gb2312_chinese_ci": "Simplified Chinese, case-insensitive",
+
+
+    "gbk_bin": "Simplified Chinese, Binary",
+    "gbk_chinese_ci": "Simplified Chinese, case-insensitive",
+
+
+    "geostd8_bin": "Georgian, Binary",
+    "geostd8_general_ci": "Georgian, case-insensitive",
+
+
+    "greek_bin": "Greek, Binary",
+    "greek_general_ci": "Greek, case-insensitive",
+
+
+    "hebrew_bin": "Hebrew, Binary",
+    "hebrew_general_ci": "Hebrew, case-insensitive",
+
+
+    "hp8_bin": "West European (multilingual), Binary",
+    "hp8_english_ci": "English, case-insensitive",
+
+
+    "keybcs2_bin": "Czech-Slovak, Binary",
+    "keybcs2_general_ci": "Czech-Slovak, case-insensitive",
+
+
+    "koi8r_bin": "Russian, Binary",
+    "koi8r_general_ci": "Russian, case-insensitive",
+
+
+    "koi8u_bin": "Ukrainian, Binary",
+    "koi8u_general_ci": "Ukrainian, case-insensitive",
+
+
+    "latin1_bin": "West European (multilingual), Binary",
+    "latin1_danish_ci": "Danish, case-insensitive",
+    "latin1_general_ci": "West European (multilingual), case-insensitive",
+    "latin1_general_cs": "West European (multilingual), case-sensitive",
+    "latin1_german1_ci": "German (dictionary), case-insensitive",
+    "latin1_german2_ci": "German (phone book), case-insensitive",
+    "latin1_spanish_ci": "Spanish, case-insensitive",
+    "latin1_swedish_ci": "Swedish, case-insensitive",
+
+
+    "latin2_bin": "Central European (multilingual), Binary",
+    "latin2_croatian_ci": "Croatian, case-insensitive",
+    "latin2_czech_cs": "Czech, case-sensitive",
+    "latin2_general_ci": "Central European (multilingual), case-insensitive",
+    "latin2_hungarian_ci": "Hungarian, case-insensitive",
+
+
+    "latin5_bin": "Turkish, Binary",
+    "latin5_turkish_ci": "Turkish, case-insensitive",
+
+
+    "latin7_bin": "Baltic (multilingual), Binary",
+    "latin7_estonian_cs": "Estonian, case-sensitive",
+    "latin7_general_ci": "Baltic (multilingual), case-insensitive",
+    "latin7_general_cs": "Baltic (multilingual), case-sensitive",
+
+
+    "macce_bin": "Central European (multilingual), Binary",
+    "macce_general_ci": "Central European (multilingual), case-insensitive",
+
+
+    "macroman_bin": "West European (multilingual), Binary",
+    "macroman_general_ci": "West European (multilingual), case-insensitive",
+
+
+    "sjis_bin": "Japanese, Binary",
+    "sjis_japanese_ci": "Japanese, case-insensitive",
+
+
+    "swe7_bin": "Swedish, Binary",
+    "swe7_swedish_ci": "Swedish, case-insensitive",
+
+
+    "tis620_bin": "Thai, Binary",
+    "tis620_thai_ci": "Thai, case-insensitive",
+
+
+    "ucs2_bin": "Unicode (multilingual), Binary",
+    "ucs2_czech_ci": "Czech, case-insensitive",
+    "ucs2_danish_ci": "Danish, case-insensitive",
+    "ucs2_esperanto_ci": "Esperanto, case-insensitive",
+    "ucs2_estonian_ci": "Estonian, case-insensitive",
+    "ucs2_general_ci": "Unicode (multilingual), case-insensitive",
+    "ucs2_general_mysql500_ci": "Unicode (multilingual)",
+    "ucs2_hungarian_ci": "Hungarian, case-insensitive",
+    "ucs2_icelandic_ci": "Icelandic, case-insensitive",
+    "ucs2_latvian_ci": "Latvian, case-insensitive",
+    "ucs2_lithuanian_ci": "Lithuanian, case-insensitive",
+    "ucs2_persian_ci": "Persian, case-insensitive",
+    "ucs2_polish_ci": "Polish, case-insensitive",
+    "ucs2_roman_ci": "West European, case-insensitive",
+    "ucs2_romanian_ci": "Romanian, case-insensitive",
+    "ucs2_sinhala_ci": "unknown, case-insensitive",
+    "ucs2_slovak_ci": "Slovak, case-insensitive",
+    "ucs2_slovenian_ci": "Slovenian, case-insensitive",
+    "ucs2_spanish2_ci": "Traditional Spanish, case-insensitive",
+    "ucs2_spanish_ci": "Spanish, case-insensitive",
+    "ucs2_swedish_ci": "Swedish, case-insensitive",
+    "ucs2_turkish_ci": "Turkish, case-insensitive",
+    "ucs2_unicode_ci": "Unicode (multilingual), case-insensitive",
+
+
+    "ujis_bin": "Japanese, Binary",
+    "ujis_japanese_ci": "Japanese, case-insensitive",
+
+
+    "utf16_bin": "unknown, Binary",
+    "utf16_czech_ci": "Czech, case-insensitive",
+    "utf16_danish_ci": "Danish, case-insensitive",
+    "utf16_esperanto_ci": "Esperanto, case-insensitive",
+    "utf16_estonian_ci": "Estonian, case-insensitive",
+    "utf16_general_ci": "unknown, case-insensitive",
+    "utf16_hungarian_ci": "Hungarian, case-insensitive",
+    "utf16_icelandic_ci": "Icelandic, case-insensitive",
+    "utf16_latvian_ci": "Latvian, case-insensitive",
+    "utf16_lithuanian_ci": "Lithuanian, case-insensitive",
+    "utf16_persian_ci": "Persian, case-insensitive",
+    "utf16_polish_ci": "Polish, case-insensitive",
+    "utf16_roman_ci": "West European, case-insensitive",
+    "utf16_romanian_ci": "Romanian, case-insensitive",
+    "utf16_sinhala_ci": "unknown, case-insensitive",
+    "utf16_slovak_ci": "Slovak, case-insensitive",
+    "utf16_slovenian_ci": "Slovenian, case-insensitive",
+    "utf16_spanish2_ci": "Traditional Spanish, case-insensitive",
+    "utf16_spanish_ci": "Spanish, case-insensitive",
+    "utf16_swedish_ci": "Swedish, case-insensitive",
+    "utf16_turkish_ci": "Turkish, case-insensitive",
+    "utf16_unicode_ci": "Unicode (multilingual), case-insensitive",
+
+
+    "utf32_bin": "unknown, Binary",
+    "utf32_czech_ci": "Czech, case-insensitive",
+    "utf32_danish_ci": "Danish, case-insensitive",
+    "utf32_esperanto_ci": "Esperanto, case-insensitive",
+    "utf32_estonian_ci": "Estonian, case-insensitive",
+    "utf32_general_ci": "unknown, case-insensitive",
+    "utf32_hungarian_ci": "Hungarian, case-insensitive",
+    "utf32_icelandic_ci": "Icelandic, case-insensitive",
+    "utf32_latvian_ci": "Latvian, case-insensitive",
+    "utf32_lithuanian_ci": "Lithuanian, case-insensitive",
+    "utf32_persian_ci": "Persian, case-insensitive",
+    "utf32_polish_ci": "Polish, case-insensitive",
+    "utf32_roman_ci": "West European, case-insensitive",
+    "utf32_romanian_ci": "Romanian, case-insensitive",
+    "utf32_sinhala_ci": "unknown, case-insensitive",
+    "utf32_slovak_ci": "Slovak, case-insensitive",
+    "utf32_slovenian_ci": "Slovenian, case-insensitive",
+    "utf32_spanish2_ci": "Traditional Spanish, case-insensitive",
+    "utf32_spanish_ci": "Spanish, case-insensitive",
+    "utf32_swedish_ci": "Swedish, case-insensitive",
+    "utf32_turkish_ci": "Turkish, case-insensitive",
+    "utf32_unicode_ci": "Unicode (multilingual), case-insensitive",
+
+
+    "utf8_bin": "Unicode (multilingual), Binary",
+    "utf8_czech_ci": "Czech, case-insensitive",
+    "utf8_danish_ci": "Danish, case-insensitive",
+    "utf8_esperanto_ci": "Esperanto, case-insensitive",
+    "utf8_estonian_ci": "Estonian, case-insensitive",
+    "utf8_general_ci": "Unicode (multilingual), case-insensitive",
+    "utf8_general_mysql500_ci": "Unicode (multilingual)",
+    "utf8_hungarian_ci": "Hungarian, case-insensitive",
+    "utf8_icelandic_ci": "Icelandic, case-insensitive",
+    "utf8_latvian_ci": "Latvian, case-insensitive",
+    "utf8_lithuanian_ci": "Lithuanian, case-insensitive",
+    "utf8_persian_ci": "Persian, case-insensitive",
+    "utf8_polish_ci": "Polish, case-insensitive",
+    "utf8_roman_ci": "West European, case-insensitive",
+    "utf8_romanian_ci": "Romanian, case-insensitive",
+    "utf8_sinhala_ci": "unknown, case-insensitive",
+    "utf8_slovak_ci": "Slovak, case-insensitive",
+    "utf8_slovenian_ci": "Slovenian, case-insensitive",
+    "utf8_spanish2_ci": "Traditional Spanish, case-insensitive",
+    "utf8_spanish_ci": "Spanish, case-insensitive",
+    "utf8_swedish_ci": "Swedish, case-insensitive",
+    "utf8_turkish_ci": "Turkish, case-insensitive",
+    "utf8_unicode_ci": "Unicode (multilingual), case-insensitive",
+
+
+    "utf8mb4_bin": "unknown, Binary",
+    "utf8mb4_czech_ci": "Czech, case-insensitive",
+    "utf8mb4_danish_ci": "Danish, case-insensitive",
+    "utf8mb4_esperanto_ci": "Esperanto, case-insensitive",
+    "utf8mb4_estonian_ci": "Estonian, case-insensitive",
+    "utf8mb4_general_ci": "unknown, case-insensitive",
+    "utf8mb4_hungarian_ci": "Hungarian, case-insensitive",
+    "utf8mb4_icelandic_ci": "Icelandic, case-insensitive",
+    "utf8mb4_latvian_ci": "Latvian, case-insensitive",
+    "utf8mb4_lithuanian_ci": "Lithuanian, case-insensitive",
+    "utf8mb4_persian_ci": "Persian, case-insensitive",
+    "utf8mb4_polish_ci": "Polish, case-insensitive",
+    "utf8mb4_roman_ci": "West European, case-insensitive",
+    "utf8mb4_romanian_ci": "Romanian, case-insensitive",
+    "utf8mb4_sinhala_ci": "unknown, case-insensitive",
+    "utf8mb4_slovak_ci": "Slovak, case-insensitive",
+    "utf8mb4_slovenian_ci": "Slovenian, case-insensitive",
+    "utf8mb4_spanish2_ci": "Traditional Spanish, case-insensitive",
+    "utf8mb4_spanish_ci": "Spanish, case-insensitive",
+    "utf8mb4_swedish_ci": "Swedish, case-insensitive",
+    "utf8mb4_turkish_ci": "Turkish, case-insensitive",
+    "utf8mb4_unicode_ci": "Unicode (multilingual), case-insensitive",
+
+}
